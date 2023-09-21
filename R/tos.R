711eca3b837b57d96cf516e06e6c0451f4cfd609
Madina <m.hasan@sheffield.ac.uk>
2023-09-21 16:29:49 +0100

    adding tos parser script

---
+++ R/tos.R
@@ -0,0 +1,28 @@
+#' Load TOS files
+#'
+#' @param xls_file str Path File to Excel file from which TOS are to be loaded.
+#' @param sheet str Name of worksheet to be loaded.
+#' @param sheet_number int Number of worksheet to be loaded
+#' @export
+load_xls <- function(xls_file, sheet = "HES APC TOS", sheet_number = NA) {
+  # Get col names
+  col_names <- as.character(read_excel(xls_file, sheet, skip = 1, n_max = 1, col_names = FALSE))
+  # Get data only and attached the cols names
+  my_data <- read_excel(xls_file, sheet, skip = 2, col_names = col_names)
+  var_list <- c("Field", "Field name", "Format", "Availability", "Values")
+  my_data2 <- select(my_data, all_of(var_list))
+  my_data2
+}
+
+#' write dataframe to json
+#'
+#' @param df dataframe(tibble)
+#' @param outputdir str
+#' @param json_file str
+#' @export
+df2json <- function(df, outputdir, json_file) {
+  if (!dir.exists(file.path(outputdir))) {
+    dir.create(outputdir)
+  }
+  write_json(df, file.path(outputdir, json_file))
+}
