d1ffa31351b721c4cc76091876fea12579c60efe
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-03 11:33:28 +0100

    Add example TOS metadata JSON File

--- R/extdata.R
+++ R/extdata.R
@@ -1,8 +1,9 @@
 # Data set documentation
 # This file documents the data sets contained in inst/extdata
 
-#' HES Admitted Patient Care artificial data set
+#' HES Admitted Patient Care (APC) artificial data set
 #'
+#' Hospital Episode Statistics (HES)
 #' [NHS Artificial data pilot](https://digital.nhs.uk/services/artificial-data)
 #'
 #' The documentation and the artificial data files are © Crown copyright and available under the terms of the
@@ -13,5 +14,18 @@
 #' @source https://digital.nhs.uk/services/artificial-data
 #'
 #' @section artificial_hes_apc_0102.csv:
+#' One row per care event.
+#'
+NULL
+
+#' HES Admitted Patient Care (APC) metadata
+#' 
+#' This is extracted from the Technical Output Specification (TOS) for the Hospital Episode Statistics (HES) data.
+#'
+#' @name hes_apc_metadata
+#'
+#' @source https://digital.nhs.uk/data-and-information/data-tools-and-services/data-services/hospital-episode-statistics/hospital-episode-statistics-data-dictionary
+#' @section hes_apc_metadata.json:
+#' Each item is a field in this data set.
 #'
 NULL
