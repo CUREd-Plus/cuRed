8becc7e1879f62c404db681465a628306c0b21e2
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-06 16:22:30 +0100

    build

--- tests/testthat/test-get_data_types.R
+++ tests/testthat/test-get_data_types.R
@@ -6,7 +6,7 @@ test_that("Get the SQL data type for each field in a data set", {
   metadata_json <- '[{"Field": "ADMIDATE", "Format": "Date(YYYY-MM-DD)"}]'
   metadata <- as.data.frame(jsonlite::fromJSON(metadata_json))
   field_names <- get_data_types(metadata = metadata)
-  
+
   # Evaluate result
   correct_field_names <- list(ADMIDATE = "DATE")
   testthat::expect_equal(field_names, correct_field_names)
