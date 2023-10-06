e2d5d075b2e83f1b188221f6287e45a31a166320
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-06 15:37:06 +0100

    Enable testthat. Add unit test for convert_json_to_struct()

---
+++ tests/testthat/test-convert_json_to_struct.R
@@ -0,0 +1,9 @@
+test_that("Convert JSON data to an SQL struct", {
+  data <- '{"key": "value"}'
+    
+  struct <- convert_json_to_struct(data)
+  
+  correct_struct <- "{'key': 'value'}"
+  
+  expect_equal(struct, correct_struct)
+})
