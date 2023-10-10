f854836dfe6cf750a3d918d916a817e30ff584fb
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-10 13:27:36 +0100

    Add read_parquet() function

---
+++ R/read_parquet.R
@@ -0,0 +1,15 @@
+#' Load an Apache Parquet file into an R data frame.
+#' 
+#' https://duckdb.org/docs/data/parquet/overview#read_parquet-function
+#' 
+#' @param file_path String. Source data file path.
+#' @export
+#' @returns Data frame. Data file contents.
+read_parquet <- function(file_path) {
+  # Check file exists
+  file_path <- normalizePath(file.path(file_path), mustWork = TRUE)
+  # Parse Parquet file
+  query <- stringr::str_glue("SELECT * FROM read_parquet('{file_path}')")
+  data <- get_query(query)
+  return(data)
+}
