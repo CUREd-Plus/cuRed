5e61b5eb84bb6279ca10f2578a945bba06a1a97b
Madina <m.hasan@sheffield.ac.uk>
2023-10-10 11:26:18 +0100

    removing empty lines

--- R/tos.R
+++ R/tos.R
@@ -4,12 +4,13 @@
 #' @param sheet str Name of worksheet to be loaded.
 #' @param sheet_number int Number of worksheet to be loaded
 #' @export
+#'
 load_xls <- function(xls_file, sheet = "HES APC TOS", sheet_number = NA) {
+  myCols <- as.character(read_excel(xls_file, sheet, n_max = 2, skip = 1, col_names = FALSE))
+  print(myCols)
   # Get data only and attached the cols names
   var_list <- c("Field", "Field name", "Format", "Availability", "Values")
-  my_data <- read_excel(xls_file, sheet, skip = 2, col_names = var_list)
-  my_data2 <- select(my_data, all_of(var_list))
-  my_data2
+  my_data <- read_excel(xls_file, sheet, skip = 2, col_names = myCols)
 }
 
 #' write dataframe to json
