7311160f272b92c3e6bcede4a956f8b8e7e0aee7
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-10 13:20:14 +0100

    Add extdata_path() function

---
+++ R/extdata_path.R
@@ -0,0 +1,8 @@
+#' Get absolute path for an extdata file in this package
+#' https://r-pkgs.org/data.html#sec-data-extdata
+#' @param path String. Path.
+#' @param mustWork Logical. Raise an error if the file is not found.
+extdata_path <- function(path, mustWork=TRUE, ...){
+  path <- normalizePath(system.file("extdata", path, package = "cuRed"), mustWork = mustWork, ...)
+  return(path)
+}
