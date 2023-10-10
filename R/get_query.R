05c0062e7b52ac8479eb6f9e0d51f5015bb3c7a9
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-10 10:51:38 +0100

    precommit

--- R/get_query.R
+++ R/get_query.R
@@ -5,12 +5,12 @@
 get_query <- function(query) {
   # Connect to an in-memory database
   con <- DBI::dbConnect(duckdb::duckdb(), dbdir = ":memory:")
-  
+
   # Run the query
   data <- DBI::dbGetQuery(con, query)
-  
+
   # Close the database connection
   DBI::dbDisconnect(con, shutdown = TRUE)
-  
+
   return(data)
 }
