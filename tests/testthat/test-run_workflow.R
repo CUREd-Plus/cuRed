e9287c4426a2aa4d29b9519ae7f977936e6f454d
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-17 17:25:04 +0100

    Fix temp file and download

--- tests/testthat/test-run_workflow.R
+++ tests/testthat/test-run_workflow.R
@@ -1,16 +1,20 @@
+library(fs)
+
 # Automatically test the entire workflow for a single data set
 test_that("run_workflow", {
   data_set_id <- "apc"
-  raw_data_dir <- normalizePath(extdata_path("data/apc/raw"), mustWork = TRUE)
+  raw_data_dir <- extdata_path("data/apc/raw", mustWork = TRUE)
   staging_dir <- temp_dir()
+  fs::dir_create(staging_dir)
 
   # Download Technical Output Specification (TOS) spreadsheet for  Hospital Episode Statistics (HES)
   # See: https://digital.nhs.uk/data-and-information/data-tools-and-services/data-services/hospital-episode-statistics/hospital-episode-statistics-data-dictionary
   url <- "https://digital.nhs.uk/binaries/content/assets/website-assets/data-and-information/data-tools-and-services/data-services/hospital-episode-statistics/hes-data-dictionary/hes-tos-v1.15.xlsx"
   # https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/tempfile
-  tos_path <- tempfile(tmpdir = staging_dir, fileext = ".xlsx")
+  tos_path <- fs::file_temp(tmp_dir = staging_dir, ext = ".xlsx")
   # https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/download.file
-  download.file(url, method = "auto", destfile = tos_path)
+  # We must specify mode = "wb" for this to work on Windows
+  download.file(url, method = "auto", destfile = tos_path, mode = "wb")
 
   # Run the workflow
   expect_no_error(
