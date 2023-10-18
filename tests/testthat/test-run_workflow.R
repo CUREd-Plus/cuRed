710a293026922b530e82824efdc78abc4954667f
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-18 09:11:15 +0100

    fix: Workflow test, set download mode

--- tests/testthat/test-run_workflow.R
+++ tests/testthat/test-run_workflow.R
@@ -1,7 +1,7 @@
 # Automatically test the entire workflow for a single data set
 test_that("run_workflow", {
   data_set_id <- "apc"
-  raw_data_dir <- normalizePath(extdata_path("data/apc/raw"), mustWork = TRUE)
+  raw_data_dir <- extdata_path("data/apc/raw", mustWork = TRUE)
   staging_dir <- temp_dir()
 
   # Download Technical Output Specification (TOS) spreadsheet for  Hospital Episode Statistics (HES)
@@ -10,7 +10,7 @@ test_that("run_workflow", {
   # https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/tempfile
   tos_path <- tempfile(tmpdir = staging_dir, fileext = ".xlsx")
   # https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/download.file
-  download.file(url, method = "auto", destfile = tos_path)
+  download.file(url, method = "auto", destfile = tos_path, mode = "wb")
 
   # Run the workflow
   expect_no_error(
