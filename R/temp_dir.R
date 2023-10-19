bb04147d87e5e95875a34a18d2890039422c5967
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-19 16:28:25 +0100

    Clarify temp_dir usage

--- R/temp_dir.R
+++ R/temp_dir.R
@@ -1,4 +1,6 @@
-#' Create a temporary directory
+#' Create a random path for a temporary directory.
+#' 
+#' This *doesn't* create the directory. To do so, use dir.create.
 #'
 #' R can't handle long path names on Windows, so use a short path.
 #' See [issue 75](https://github.com/CUREd-Plus/cuRed/issues/75).
@@ -7,6 +9,10 @@
 #' https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/tempfile
 #'
 #' @param check logical indicating if temp_dir() should be checked and recreated if no longer valid.
+#' 
+#' @examples
+#' my_directory <- temp_dir()
+#' dir.create(my_directory)
 #' 
 temp_dir <- function(check = FALSE) {
   if (.Platform$OS.type == "windows") {
