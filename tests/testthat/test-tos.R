fe9b5173d4a4073d74947310507ae07adcf8737b
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-19 15:00:51 +0100

    specify sheet name

--- tests/testthat/test-tos.R
+++ tests/testthat/test-tos.R
@@ -1,4 +1,6 @@
 test_that("parse_tos", {
+  sheet <- "HES APC TOS"
+  
   # Download Technical Output Specification (TOS) spreadsheet for  Hospital Episode Statistics (HES)
   # See: https://digital.nhs.uk/data-and-information/data-tools-and-services/data-services/hospital-episode-statistics/hospital-episode-statistics-data-dictionary
   url <- "https://digital.nhs.uk/binaries/content/assets/website-assets/data-and-information/data-tools-and-services/data-services/hospital-episode-statistics/hes-data-dictionary/hes-tos-v1.15.xlsx"
@@ -9,6 +11,6 @@ test_that("parse_tos", {
 
   # Parse TOS
   expect_no_error(
-    parse_tos(tos_path)
+    parse_tos(tos_path, sheet = sheet)
   )
 })
