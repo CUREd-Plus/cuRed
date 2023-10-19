5d59c8aaaa020242ff970d533836bd4db6166639
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-19 15:35:29 +0100

    Force windows backslash

--- R/temp_dir.R
+++ R/temp_dir.R
@@ -4,11 +4,11 @@
 #' See [issue 75](https://github.com/CUREd-Plus/cuRed/issues/75).
 #'
 #' https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/tempfile
-temp_dir <- function(tmpdir = "C:/temp", check = FALSE) {
+temp_dir <- function(tmpdir = "C:\\temp", check = FALSE) {
   if (.Platform$OS.type == "windows") {
     # Build a path with fewer characters
     dir_name = basename(tempdir())
-    path <- file.path(tmpdir, dir_name)
+    path <- file.path(tmpdir, dir_name, fsep = "\\")
   } else {
     path <- tempdir(check = check)
   }
