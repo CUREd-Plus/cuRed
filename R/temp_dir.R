fc96a1ebefeb9a21a76656abb86b063816d44bbb
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-19 16:14:11 +0100

    -

--- R/temp_dir.R
+++ R/temp_dir.R
@@ -5,13 +5,13 @@
 #'
 #' This is a wrapper for tempdir(), which is documented here:
 #' https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/tempfile
-#' 
+#'
 #' @param check logical indicating if temp_dir() should be checked and recreated if no longer valid.
 #' 
 temp_dir <- function(check = FALSE) {
   if (.Platform$OS.type == "windows") {
     # Build a path with fewer characters
-    dir_name = basename(tempdir())
+    dir_name <- basename(tempdir())
     path <- file.path("C:\\temp", dir_name, fsep = "\\")
     if (check) {
       stop("check is not implemented for windows")
