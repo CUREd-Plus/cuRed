8f67936d143b2f94d03664fd84d6c6d12f5e0132
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-20 14:27:09 +0100

    Add function to retrieve sample data

--- R/count_rows.R
+++ R/count_rows.R
@@ -1,13 +1,14 @@
 #' Count the number of rows in the input data set.
 #'
 #' @param path character File path of a Parquet file.
+#' @param read_func character, name of DuckDB function that loads the file
 #'
 #' @export
 #'
 #' @returns The number of rows in the input data file.
 #'
-count_rows <- function(path) {
-  query <- stringr::str_glue("SELECT COUNT(*) FROM read_parquet('{path}')")
+count_rows <- function(path, read_func = "read_parquet") {
+  query <- stringr::str_glue("SELECT COUNT(*) FROM {read_func}('{path}')")
   result <- get_query(query)
   count <- as.integer(result[[1]])
   return(count)
