55b2758b14663aa51e8b8e52030202f1b9274ffe
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-24 09:27:45 +0100

    chore: code tidy and doc-strings

--- R/extdata_path.R
+++ R/extdata_path.R
@@ -2,7 +2,10 @@
 #' https://r-pkgs.org/data.html#sec-data-extdata
 #' @param path String. Path.
 #' @param mustWork Logical. Raise an error if the file is not found.
-extdata_path <- function(path = "", mustWork = TRUE) {
-  path <- normalizePath(system.file("extdata", path, package = "cuRed"), mustWork = mustWork)
+extdata_path <- function(filename = "", mustWork = TRUE) {
+  # Get path of a file within inst/extdata
+  path = system.file("extdata", filename, package = "cuRed")
+  # Ensure the path is absolute and that the file exists
+  path <- normalizePath(path, mustWork = mustWork)
   return(path)
 }
--- R/main.R
+++ R/main.R
@@ -17,24 +17,26 @@ library(cli)
 main <- function(data_sets_path = NA, patient_path, demographics_path) {
   # Set the file path of the configuration file
   if (is.na(data_sets_path)) {
-    data_sets_path <- system.file("extdata", "data_sets.json", package = "cuRed", mustWork = TRUE)
+    data_sets_path = extdata_path("data_sets.json")
+  } else {
+    # Check that the specified file exists
+    data_sets_path = normalizePath(data_sets_path, mustWork = TRUE)
   }
-
-  data_sets_path <- normalizePath(file.path(data_sets_path), mustWork = TRUE)
 
   # Load the configuration file
   # This is a list of objects, each represents a data set
   data_sets <- as.data.frame(jsonlite::fromJSON(data_sets_path))
-  data_sets <- as.data.frame(jsonlite::fromJSON())
 
   # Iterate over data sets
   for (i in seq_len(nrow(data_sets))) {
+    # Get the details of this data set
     data_set_id <- as.character(data_sets$id[i])
     raw_data_dir <- normalizePath(file.path(data_sets$raw_data_dir[i]), mustWork = TRUE)
     metadata_path <- normalizePath(file.path(data_sets$metadata_path[i]), mustWork = TRUE)
     sheet <- data_sets$sheet[i]
     staging_dir <- normalizePath(file.path(data_sets$staging_dir[i]), mustWork = FALSE)
 
+    # Inform user what's happening
     cli::cli_alert_info("Running workflow for data set '{data_set_id}'")
     cli::cli_alert_info("Raw data directory '{raw_data_dir}'")
 
