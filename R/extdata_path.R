e3613a3e4c93e91e24307e06544ff11140adfa1a
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-10-24 09:47:42 +0100

    chore: code tidy and doc-strings

--- R/extdata_path.R
+++ R/extdata_path.R
@@ -1,10 +1,10 @@
 #' Get absolute path for an extdata file in this package
 #' https://r-pkgs.org/data.html#sec-data-extdata
-#' @param path String. Path.
-#' @param mustWork Logical. Raise an error if the file is not found.
-extdata_path <- function(filename = "", mustWork = TRUE) {
+#' @param path Character. Path of file within inst/extdata
+#' @param mustWork Logical. Raise an error if the file is not found
+extdata_path <- function(path = "", mustWork = TRUE) {
   # Get path of a file within inst/extdata
-  path = system.file("extdata", filename, package = "cuRed")
+  path = system.file("extdata", path, package = "cuRed")
   # Ensure the path is absolute and that the file exists
   path <- normalizePath(path, mustWork = mustWork)
   return(path)
