0c42f37cd6894299ad65fda48a52c99f798d8a33
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-11-09 15:37:03 +0000

    Join to MHS101 Referral table

--- inst/extdata/queries/mhsds.sql
+++ inst/extdata/queries/mhsds.sql
@@ -3,12 +3,14 @@ Mental Health Services Data Set (MHSDS) denormalisation query.
 
 This is an SQL query that will convert the many tables in the MHSDS into a single table.
 
+The table and field names are determined by the MHSDS v5.0 Technical Output Specification (TOS).
+
 For more information about the MHSDS, see:
 https://digital.nhs.uk/data-and-information/data-collections-and-data-sets/data-sets/mental-health-services-data-set
 
 */
 SELECT
-  -- Ward Stay
+  -- MHS502 Ward Stay
    WardStay.WardStayId -- M502915 WARD STAY IDENTIFIER
   ,WardStay.StartDateWardStay -- START DATE (WARD STAY)
   ,WardStay.StartTimeWardStay
@@ -25,9 +27,9 @@ SELECT
   ,WardStay.HospitalBedTypeMH
   ,WardStay.SpecialisedMHServiceCode
   ,WardStay.WardCode
-  -- Hospital Provider Spell
+
+  -- MHS501 Hospital Provider Spel
   ,WardStay.HospProvSpellID -- M502160 HOSPITAL PROVIDER SPELL IDENTIFIER
-  ,HospitalProviderSpell.ServiceRequestId -- M501902 SERVICE REQUEST IDENTIFIER
   ,HospitalProviderSpell.DecidedToAdmitDate
   ,HospitalProviderSpell.DecidedToAdmitTime
   ,HospitalProviderSpell.StartDateHospProvSpell
@@ -45,6 +47,32 @@ SELECT
   ,HospitalProviderSpell.PostcodeDischDestHospProvSpell
   ,HospitalProviderSpell.TransformingCareInd
   ,HospitalProviderSpell.TransformingCareCategory
+  
+  -- MHS101 Service or Team Referral
+  ,HospitalProviderSpell.ServiceRequestId -- M501902 SERVICE REQUEST IDENTIFIER
+  ,Referral.LocalPatientId -- M101901 LOCAL PATIENT IDENTIFIER
+  ,Referral.OrgIDComm -- M101922 ORGANISATION IDENTIFIER (CODE OF COMMISSIONER)
+  ,Referral.ReferralRequestReceivedDate -- M101010 REFERRAL REQUEST RECEIVED DATE
+  ,Referral.ReferralRequestReceivedTime
+  ,Referral.NHSServAgreeLineNum
+  ,Referral.SpecialisedMHServiceCode
+  ,Referral.SourceOfReferralMH
+  ,Referral.OrgIDReferring
+  ,Referral.ReferringCareProfessionalStaffGroup
+  ,Referral.ClinRespPriorityType
+  ,Referral.PrimReasonReferralMH
+  ,Referral.ReasonOAT
+  ,Referral.DecisionToTreatDate
+  ,Referral.DecisionToTreatTime
+  ,Referral.DischPlanCreationDate
+  ,Referral.DischPlanCreationTime
+  ,Referral.DischPlanLastUpdatedDate
+  ,Referral.DischPlanLastUpdatedTime
+  ,Referral.ServDischDate
+  ,Referral.ServDischTime
+  
 FROM MHS502WardStay AS WardStay;
-LEFT JOIN MHS501HospProvSpell AS HospitalProviderSpell
+LEFT INNER JOIN MHS501HospProvSpell AS HospitalProviderSpell
   ON WardStay.HospProvSpellID = HospitalProviderSpell.HospProvSpellID
+LEFT INNER JOIN MHS101Referral AS Referral
+  ON HospitalProviderSpell.ServiceRequestId = Referral.ServiceRequestId
