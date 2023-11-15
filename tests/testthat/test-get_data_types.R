6014bd0d92213efc0ed04017877cfa6bacb97e08
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-11-15 15:11:29 +0000

    Remove testthat import

--- tests/testthat/test-get_data_types.R
+++ tests/testthat/test-get_data_types.R
@@ -1,7 +1,6 @@
-library(testthat)
 library(jsonlite)
 
-test_that("Get the SQL data type for each field in a data set", {
+test_that("get_data_types", {
   # Run the function with example metadata
   metadata_json <- '[{"Field": "ADMIDATE", "Format": "Date(YYYY-MM-DD)"}]'
   metadata <- as.data.frame(jsonlite::fromJSON(metadata_json))
@@ -9,5 +8,5 @@ test_that("Get the SQL data type for each field in a data set", {
 
   # Evaluate result
   correct_field_names <- list(ADMIDATE = "DATE")
-  testthat::expect_equal(field_names, correct_field_names)
+  expect_equal(field_names, correct_field_names)
 })
