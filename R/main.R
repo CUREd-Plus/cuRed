182e24c8ea8b4dd0439f9459b1be8212083f13b3
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-12-05 13:01:20 +0000

    Add simple audit log for package version

--- R/main.R
+++ R/main.R
@@ -4,19 +4,24 @@ library(cli)
 #'
 #' @description
 #' The function executes the entire data pipeline for the specified data set.
-#' 
+#'
 #' @param data_set_id character. Data set identifier e.g. "apc", "yas_epr", "op"
 #'
 #' @export
 #'
 main <- function(data_set_id) {
-  
+
+  # Audit log
+  cured_version <- utils::packageVersion("cuRed")
+  current_timestamp <- Sys.time()
+  cli::cli_inform("{current_timestamp} running cuRed package version {cured_version}")
+
   # Load global options
   # https://rstudio.github.io/config/reference/
   config_file_path <- extdata_path("config/config.yaml")
   cli::cli_alert_info("Loading '{config_file_path}'")
   config <- config::get(file = config_file_path, use_parent = FALSE)
-  
+
   # Load data set options
   data_set_config_file_path <- extdata_path(stringr::str_glue("config/{data_set_id}.yaml"))
   cli::cli_alert_info("Loading '{data_set_config_file_path}'")
