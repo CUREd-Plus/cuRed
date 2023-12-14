b13e17b7ee5a523459b1bc37fff047e504296304
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-12-14 12:00:26 +0000

    -

--- scripts/bash/hes_op_import.sh
+++ scripts/bash/hes_op_import.sh
@@ -6,7 +6,8 @@ aws s3 sync s3://curedplus-raw.store.rcc.shef.ac.uk/NHSD-DATA/HES_OP/ ~/data/hes
 cd ~/data/hes_op/raw/
 
 # Unzip files
-sh unzip.sh
+unzip "*.zip"
+rm *.zip
 
 # Import to DuckDB
 duckdb hes_op.duckdb -s ".read ../sql/hes_op/hes_op_import.sql"
--- scripts/bash/unzip.sh
+++ scripts/bash/unzip.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
 
+# This script will unzip zip files while
+# saving space (deleting the files as we go)
+
+# If you have plenty of disk space then don't
+# use this script, just run unzip "*.zip"
+
+# Iterate over zip files
 for path in *.zip;
 do
   echo $path
+  # Decompress
   unzip $path
+  # Delete zip archive
   rm -v $path
 done
