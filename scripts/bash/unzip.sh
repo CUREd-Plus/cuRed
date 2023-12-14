f0d4cce3bd60c4a9b929cf43df0c29012d49bbe4
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-12-14 13:03:39 +0000

    run in parallel

--- scripts/bash/unzip.sh
+++ scripts/bash/unzip.sh
@@ -7,12 +7,13 @@ set -e
 # If you have plenty of disk space then don't
 # use this script, just run unzip "*.zip"
 
+target_dir=/mnt/sdd/raw/
+mkdir -p $target_dir
+
 # Iterate over zip files
 for path in *.zip;
 do
   echo $path
-  # Decompress
-  unzip $path
-  # Delete zip archive
-  rm -v $path
+  # Decompress and unzip in the background
+  unzip $path -d $target_dir && rm -v $path &
 done
