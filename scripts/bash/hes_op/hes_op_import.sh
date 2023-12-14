11c8711eba340bacacebc558356644f5dc3e6fc4
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-12-14 11:48:08 +0000

    Add HES OP scripts

--- scripts/bash/hes_op/hes_op_import.sh
+++ scripts/bash/hes_op/hes_op_import.sh
@@ -2,7 +2,11 @@
 set -e
 
 # Download data
+aws s3 sync s3://curedplus-raw.store.rcc.shef.ac.uk/NHSD-DATA/HES_OP/ ~/data/hes_op/raw/
+cd ~/data/hes_op/raw/
 
 # Unzip files
+sh unzip.sh
 
 # Import to DuckDB
+duckdb -s ".read hes_op_import.sql"
