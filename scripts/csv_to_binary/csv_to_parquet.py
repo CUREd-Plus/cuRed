5fda308ba23d82a6915bfe8da9246bd3c8c4a455
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-12-20 16:41:23 +0000

    add partitioning

--- scripts/csv_to_binary/csv_to_parquet.py
+++ scripts/csv_to_binary/csv_to_parquet.py
@@ -41,7 +41,7 @@ def get_args():
 
     # Output options
     parser.add_argument('output_dir', type=Path, help='Output data directory path')
-    parser.add_argument('-k', '--partition_key')
+    parser.add_argument('-k', '--partition_key', required=True, help="Input column name")
 
     # Performance options
     parser.add_argument('-i', '--io_thread_count', type=int, help='IO thread count', default=ARROW_IO_THREADS)
@@ -100,6 +100,11 @@ def main():
 
     # Write Parquet format
     output_dir = args.output_dir.absolute()
+
+    partitioning = pyarrow.dataset.partitioning(
+        schema=pyarrow.schema([(args.partition_key, pyarrow.string())]),
+        flavor='hive',
+    )
 
     logger.info("Writing dataset '%s'", output_dir)
     t0 = time.time()
