567355dc0c3b0902f63d0e80a986108df8961f7a
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-12-21 14:10:52 +0000

    Add 7-zip script

--- scripts/bash/unzip.sh
+++ scripts/bash/unzip.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 set -e
 
+# Usage:
+# sh unzip.sh ~/data/path
+
 # This script will unzip zip files while
 # saving space (deleting the files as we go)
 
 # If you have plenty of disk space then don't
 # use this script, just run unzip "*.zip"
 
-target_dir=.
-mkdir -p $target_dir
+# Destination folder. Default: current directory
+target_dir="${1:=.}"
+
+# Create destination folder
+mkdir -vp $target_dir
 
 # Iterate over zip files
 for path in *.zip;
 do
   echo $path
-  # Decompress and unzip in the background
-  unzip $path -d $target_dir && rm -v $path &
+  # Decompress in the background
+  7zz e "*.zip" -o"$target_dir" &
 done
