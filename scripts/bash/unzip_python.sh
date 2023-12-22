a0f50ee50e3fa40b821b9ad6b00af06a790c86b9
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-12-22 11:50:19 +0000

    Show ZIP file info

--- scripts/bash/unzip_python.sh
+++ scripts/bash/unzip_python.sh
@@ -12,10 +12,16 @@ target_dir="${2:=.}"
 # Create destination folder
 mkdir -vp $target_dir
 
+python3.11 -c 'import zlib; print("zlib", zlib.ZLIB_VERSION)'
+python3.11 -c 'import zipfile; print("ZIP_DEFLATED", zipfile.ZIP_DEFLATED)'
+
 # Iterate over zip files
 for path in $source_dir/*.zip;
 do
-  echo $path
+  # Verify ZIP file
+  python3.11 -m zipfile --test $path
+  # Show file contents
+  python3.11 -m zipfile --list $path
   # Decompress in the background
   # https://docs.python.org/3/library/zipfile.html#command-line-interface
   python3.11 -m zipfile --decompress --time -e $path $target_dir &
